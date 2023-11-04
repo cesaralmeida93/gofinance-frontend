9e46e2e91cf8e688d42c311f10fce11cd561d2d6
César Almeida <cesar.almeida@vocedm.com.br>
2023-11-04 19:54:50 -0300

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from '../styles/global'
+import type { AppProps } from 'next/app'
+import { darkTheme } from '@/styles/themes/dark';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <GlobalStyles />
+    </ThemeProvider>
+  );
+}
